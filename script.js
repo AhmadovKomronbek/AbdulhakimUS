0831d51a9778d4e7971cf84cb33c84e44bc44d85
Komronbek <138858973+AhmadovKomronbek@users.noreply.github.com>
2023-11-18 19:39:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".spinner-box").addClass("d-none");
+    $(".body").removeClass("d-none");
+    // setTimeout( function(){
+    //     $(".spinner-box").addClass("d-none");
+    //     $(".body").removeClass("d-none");
+    // }, 1500);
+});
